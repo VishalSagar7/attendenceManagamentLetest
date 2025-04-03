6c39fd958b5ee39aa6a678b975b17019b4cda247
VishalSagar7 <155461251+VishalSagar7@users.noreply.github.com>
2025-04-03 18:16:43 +0530

    changed the backend add

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,2 @@
-export const api_endpoint = "http://localhost:3001";
+// export const api_endpoint = "http://localhost:3001";
+export const api_endpoint = "https://attendencemanagamentletest.onrender.com";
