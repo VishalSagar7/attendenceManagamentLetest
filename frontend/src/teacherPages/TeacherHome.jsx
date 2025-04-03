ee12525a48e08e2d52ee3fe025cfa13488960bb5
VishalSagar7 <155461251+VishalSagar7@users.noreply.github.com>
2025-04-03 16:30:51 +0530

    commit 5

--- frontend/src/teacherPages/TeacherHome.jsx
+++ frontend/src/teacherPages/TeacherHome.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
 
-import TeacherSidebar from '../teacherComponents/TeacherSidebar.jsx'
 import { Outlet } from 'react-router-dom'
+import TeacherSidebar from "../teacherComponents/TeacherSidebar.jsx"
 
 const TeacherHome = () => {
   return (
     <div className='flex'>
       <TeacherSidebar />
-      <Outlet/>
+      <Outlet />
     </div>
   )
 }
